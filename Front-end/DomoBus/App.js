74c60a032a455156dd711f792b69ac103bd23e3b
alejandroRachadell <alejandro.rachadell@gmail.com>
2020-05-06 15:35:48 +0100

    Minor changes, added picker for sort type and prototype header on HomeScreen.

--- Front-end/DomoBus/App.js
+++ Front-end/DomoBus/App.js
@@ -1,157 +1,193 @@
 // In App.js in a new project
 
 import * as React from 'react';
+import {View, Button, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {Icon} from 'react-native-vector-icons/dist/MaterialIcons';
 import AsyncStorage from '@react-native-community/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './src/Screens/Login';
 import HomeScreen from './src/Screens/Main';
 import SplashScreen from './src/Screens/Splash';
 import AuthContext from './src/API/AuthContext';
 import LoginContext from './src/API/LoginContext';
 import UserContext from './src/API/UserContext';
-import { fetchUsers, fetchTypes, fetchProperties, fetchDivisionsInHome, fetchHomes, fetchDevicesInHome } from './src/API/Api';
+import {
+  fetchUsers,
+  fetchTypes,
+  fetchProperties,
+  fetchDivisionsInHome,
+  fetchHomes,
+  fetchDevicesInHome,
+} from './src/API/Api';
 
 const Stack = createStackNavigator();
 
-function App({ navigation }) {
+function App({navigation}) {
+  const [loginContext, setLoginContext] = React.useState({});
+  const [userContext, setUserContext] = React.useState({});
 
-	const [loginContext, setLoginContext] = React.useState({});
-	const [userContext, setUserContext] = React.useState({});
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    },
+  );
 
-	const [state, dispatch] = React.useReducer(
-		(prevState, action) => {
-			switch (action.type) {
-				case 'RESTORE_TOKEN':
-				return {
-					...prevState,
-					userToken: action.token,
-					isLoading: false,
-				};
-				case 'SIGN_IN':
-				return {
-					...prevState,
-					isSignout: false,
-					userToken: action.token,
-				};
-				case 'SIGN_OUT':
-				return {
-					...prevState,
-					isSignout: true,
-					userToken: null,
-				};
-			}
-		},
-		{
-			isLoading: true,
-			isSignout: false,
-			userToken: null,
-		}
-	);
+  React.useEffect(() => {
+    let user, home, context;
+    fetchUsers()
+      .then(result => (user = result))
+      .then(() => fetchHomes())
+      .then(result => (home = result))
+      .then(() => {
+        context = {
+          user: user,
+          home: home,
+        };
+        setLoginContext(context);
+      });
+  }, []);
 
-	React.useEffect(() => {
-		let user, home, context;
-		fetchUsers().then((result) => user = result)
-		.then(() => fetchHomes()).then((result) => home = result)
-			.then(() => {
-				context = {
-					user: user,
-					home: home
-				}
-				setLoginContext(context);
-			})
-	}, [])
-	
-	React.useEffect(() => {
-		// Fetch the token from storage then navigate to our appropriate place
-		const bootstrapAsync = async () => {
-			let userToken;
-		
-			try {
-				userToken = await AsyncStorage.getItem('userToken');
-			} catch (e) {
-				// Restoring token failed
-			}
-		
-			// After restoring token, we may need to validate it in production apps
-		
-			// This will switch to the App screen or Auth screen and this loading
-			// screen will be unmounted and thrown away.
-			dispatch({ type: 'RESTORE_TOKEN', token: userToken });
-		};
-	
-		bootstrapAsync();
-	}, []);
-	
-	const authContext = React.useMemo(
-		() => ({
-			signIn: async data => {
-				// In a production app, we need to send some data (usually username, password) to server and get a token
-				// We will also need to handle errors if sign in failed
-				// After getting token, we need to persist the token using `AsyncStorage`
-				// In the example, we'll use a dummy token
-				let type, property, division, device, context;
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
 
-				async function updateData() {
-					await fetchTypes().then((result) => type = result)
-					.then(() => fetchProperties()).then((result) => property = result)
-					.then(() => fetchDivisionsInHome(data.home_id)).then((result) => division = result)
-					.then(() => fetchDevicesInHome(data.home_id)).then((result) => device = result)
-					.then(() => {
-						context = {
-							type: type,
-							property: property,
-							division: division,
-							device: device
-						}
-						setUserContext(context)
-					})
-				}
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
 
+      // After restoring token, we may need to validate it in production apps
 
-				updateData();
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({type: 'RESTORE_TOKEN', token: userToken});
+    };
 
-				dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-			},
-			signOut: () => dispatch({ type: 'SIGN_OUT' }),
-			signUp: async data => {
-				// In a production app, we need to send user data to server and get a token
-				// We will also need to handle errors if sign up failed
-				// After getting token, we need to persist the token using `AsyncStorage`
-				// In the example, we'll use a dummy token
+    bootstrapAsync();
+  }, []);
 
-				dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-			},
-		}),
-		[]
-	);
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+        let type, property, division, device, context;
 
-	if (state.isLoading) {
-		// We haven't finished checking for the token yet
-		return <SplashScreen />;
-	}
-	return (
-		<AuthContext.Provider value={authContext}>
-		<LoginContext.Provider value={loginContext}>
-		<UserContext.Provider value={userContext}>
+        async function updateData() {
+          await fetchTypes()
+            .then(result => (type = result))
+            .then(() => fetchProperties())
+            .then(result => (property = result))
+            .then(() => fetchDivisionsInHome(data.home_id))
+            .then(result => (division = result))
+            .then(() => fetchDevicesInHome(data.home_id))
+            .then(result => (device = result))
+            .then(() => {
+              context = {
+                type: type,
+                property: property,
+                division: division,
+                device: device,
+              };
+              setUserContext(context);
+            });
+        }
 
-			<NavigationContainer>
-				<Stack.Navigator>
-				{state.userToken == null ? (
-					// <>
-						<Stack.Screen name="Login" component={LoginScreen} />
-					// </>
-				) : (
-					<>
-					<Stack.Screen name="Home" component={HomeScreen} />
-				</>
-				)}
-				</Stack.Navigator>
-			</NavigationContainer>
-		</UserContext.Provider>
-		</LoginContext.Provider>
-		</AuthContext.Provider>
-	);
+        updateData();
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+      signOut: () => dispatch({type: 'SIGN_OUT'}),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+    }),
+    [],
+  );
+
+  if (state.isLoading) {
+    // We haven't finished checking for the token yet
+    return <SplashScreen />;
+  }
+  return (
+    <AuthContext.Provider value={authContext}>
+      <LoginContext.Provider value={loginContext}>
+        <UserContext.Provider value={userContext}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              {state.userToken == null ? (
+                // <>
+                <Stack.Screen name="Login" component={LoginScreen} />
+              ) : (
+                // </>
+                <>
+                  <Stack.Screen name="Home" component={HomeScreen} />
+                </>
+              )}
+            </Stack.Navigator>
+          </NavigationContainer>
+        </UserContext.Provider>
+      </LoginContext.Provider>
+    </AuthContext.Provider>
+  );
 }
+
+{
+  /*HomeScreen.navigationOptions = ({navigation}) => {
+  // Your custom header
+  header: <View
+    style={{
+      flexDirection: 'row',
+      height: 80,
+    }}>
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Text> BACK </Text>
+    </TouchableOpacity>
+    <Text> My Header </Text>
+  </View>;
+};*/
+}
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: 120,
+  },
+});
 
 export default App;
--- Front-end/DomoBus/src/Screens/Main.js
+++ Front-end/DomoBus/src/Screens/Main.js
@@ -1,19 +1,166 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+import {Overlay} from 'react-native-elements';
+import {BoxShadow} from 'react-native-shadow';
+
 import UserContext from '../API/UserContext';
+import {Picker} from '@react-native-community/picker';
 
-
-function HomeScreen({ navigation }) {
-
+function HomeScreen({navigation}) {
   const context = React.useContext(UserContext);
 
-  console.log(context)  
+  console.log(context);
+
+  {
+    /*const [visible, setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  <Overlay
+          isVisible={visible}
+          onBackdropPress={toggleOverlay}
+          overlayStyle={styles.overlay}>
+          <Text style={styles.overlayText}>Sort by:</Text>
+          <FlatList
+            data={sortOptions}
+            renderItem={({item}) => (
+              <TouchableOpacity>
+                <View style={styles.listItem}>
+                  <Text style={styles.listText}>{item.text}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+            keyExtractor={item => item.key}
+          />
+        </Overlay>
+  */
+  }
+
+  const sortOptions = useState([
+    {key: 0, text: 'Name A-Z'},
+    {key: 1, text: 'Name Z-A'},
+    {key: 2, text: 'Device Type'},
+    {key: 3, text: 'Status'},
+  ]);
+  const [activeSort, setActiveSort] = useState(0);
+
+  const setSort = o => {
+    setActiveSort(o);
+  };
+
+  const shadowOpt = {
+    width: Dimensions.get('window').width,
+    height: 50,
+    backgroundColor: '#fff',
+    color: '#000',
+    border: 2,
+    radius: 3,
+    opacity: 0.1,
+    shadowOffset: {width: 1, height: 1},
+    elevation: 5,
+    x: 0,
+    y: 3,
+    style: {marginVertical: 5},
+  };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <BoxShadow setting={shadowOpt}>
+        <View style={styles.header}>
+          <View style={styles.headerLeft}>
+            <Text style={styles.headerText}>Home</Text>
+          </View>
+          <View style={styles.headerRight}>
+            <TouchableOpacity>
+              <View style={styles.headerButton}>
+                <Icon name="search" size={40} />
+              </View>
+            </TouchableOpacity>
+            <View style={styles.headerButton}>
+              <Icon name="sort" size={40} />
+              <Picker
+                selectedValue={activeSort}
+                style={{height: 50, width: 100}}
+                onValueChange={(itemValue, itemIndex) => setSort(itemValue)}>
+                <Picker.Item label="A-Z Name" value="0" />
+                <Picker.Item label="Z-A Name" value="1" />
+                <Picker.Item label="Device Type" value="2" />
+                <Picker.Item label="Status" value="3" />
+              </Picker>
+            </View>
+          </View>
+        </View>
+      </BoxShadow>
+      <View />
     </View>
   );
 }
+
+{
+  /* Ideally fontWeight for headerText would be semibold but android doesn't support this without importing the different weights for each font*/
+}
+const styles = StyleSheet.create({
+  container: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+  header: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+  },
+  headerLeft: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  headerRight: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  headerText: {
+    paddingLeft: 17,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  headerButton: {
+    paddingTop: 5,
+    paddingRight: 10,
+    flexDirection: 'row',
+  },
+  overlay: {
+    width: '90%',
+    height: '90%',
+  },
+  overlayText: {
+    fontSize: 20,
+  },
+  list: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  listItem: {
+    height: 20,
+    marginVertical: 5,
+    backgroundColor: '#000',
+  },
+  listText: {
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#fff',
+  },
+});
 
 export default HomeScreen;
